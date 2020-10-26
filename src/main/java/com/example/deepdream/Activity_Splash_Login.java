b4b48695f37a364a5e4932a1644666d2dbf28c4f
Keya Shukla <keyashukla@Keyas-MacBook-Pro.local>
2020-10-26 17:26:28 +0300

    Initial commit

---
+++ src/main/java/com/example/deepdream/Activity_Splash_Login.java
@@ -0,0 +1,14 @@
+package com.example.deepdream;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Splash_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.deepdream_splash_login);
+    }
+}
